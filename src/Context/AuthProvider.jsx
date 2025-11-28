aebad70f0d46e04689ff944c5588b94793b066eb
30526 <taiburrahamanapu@gmail.com>
2025-11-28 18:04:09 +0600

    feat: implement AuthProvider with Firebase email/password and Google authentication

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,7 +1,64 @@
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { AuthContext } from "./AuthContext";
+import auth from "../Firebase/firebase.init";
+import { useEffect, useState } from "react";
+
+const GoogleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-  const AuthInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Social Sign In
+  const socialSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, GoogleProvider);
+  };
+
+  //   create user
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   signIn with email and password
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // signOut
+  const signOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  //   observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const AuthInfo = {
+    socialSignIn,
+    createUser,
+    signInUser,
+    signOutUser,
+    user,
+    loading,
+  };
   return <AuthContext value={AuthInfo}>{children}</AuthContext>;
 };
 
